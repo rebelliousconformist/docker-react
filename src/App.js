03c2f99a49ca27cad13cee7573ee520e285bb6f5
nachiket <nachiket@nachiket-lb.linuxlab.local>
2022-02-15 12:49:27 +0100

    changed the app name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. peter 
+          Edit <code>src/App.js</code> and save to reload. pehhhter 
         </p>
         <a
           className="App-link"
